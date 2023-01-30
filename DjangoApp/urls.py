7c445058e812209aeea158ede72323410e94b1c5
heidless <rob.lockhart@yahoo.co.uk>
2023-01-30 17:08:27 +0000

    import shortener

--- DjangoApp/urls.py
+++ DjangoApp/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from other import views
+from shortener import views
 from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('other/', include('other.urls'))
+    path('other/', include('other.urls')),
 ]
