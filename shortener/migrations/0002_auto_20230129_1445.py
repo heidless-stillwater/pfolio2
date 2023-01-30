d0e796a4f330ec630885cdb969d3bb4c5389bf2b
heidless <rob.lockhart@yahoo.co.uk>
2023-01-30 19:30:19 +0000

    test 6

--- shortener/migrations/0002_auto_20230129_1445.py
+++ shortener/migrations/0002_auto_20230129_1445.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('Shortener', '0001_initial'),
+        ('shortener', '0001_initial'),
     ]
 
     operations = [
